8ec32620759cd7b902ac17a1ccdbe1253ec5b927
vvn dinesh <55079034+vvndinesh@users.noreply.github.com>
2019-09-30 12:57:55 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,17 @@
+//*VADADA VENKATA NAGA DINESH**//
+//**Git hub :- vvndinesh**//
+
+class Constructor
+{
+	public static void main(String [] arugs)
+	{
+        Repeat obj = new Repeat();
+	}
+}
+class Repeat
+{
+	public Repeat()
+	{
+        System.out.println("This is a Constructor");
+	}
+}
